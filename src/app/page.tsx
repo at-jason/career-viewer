bdb8de1854e2a23d0f80f7a9e0a6a203eca442be
at-jason <32022672+at-jason@users.noreply.github.com>
2025-01-02 15:04:30 -0500

    Update page.tsx
    background-color changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { CareerViewer } from '../components/CareerViewer.tsx';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CareerViewer />
-    </main>
+   <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#17212B]">
+  <CareerViewer />
+</main>
   );
 }
